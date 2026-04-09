7a2d564f26ca56951f63dc76da15b9a93606eacb
xtclovver <notcvnt@gmail.com>
2026-04-09 16:14:45 +0300

    feat: add locale configuration for en/ru/fa/zh-CN

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        androidResources.localeFilters += listOf("en", "ru", "fa", "zh-rCN")
     }
 
     buildTypes {
